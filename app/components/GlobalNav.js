1f24c72dd8dac0574cd207dbfe6c28380e6f2cbd
Lyman <tanleittl@163.com>
2016-02-01 17:57:54 +0800

    初步调整Nav

--- app/components/GlobalNav.js
+++ app/components/GlobalNav.js
@@ -41,38 +41,52 @@ class GlobalNav extends React.Component {
 
   render() {
     const { user,loggedIn } = this.props
-    // console.log(auth.loggedIn())
-    // console.log(this.props.loggedIn)
+
     return (
-      <Navbar inverse>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#">React-Bootstrap</a>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <NavItem eventKey={1} href="#">Link</NavItem>
-            <NavItem eventKey={2} href="#">Link</NavItem>
-            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.3}>Separated link</MenuItem>
-              <MenuItem eventKey={3.3}>Separated link</MenuItem>
-              <LinkContainer to={{ pathname: '/user/Lyman'}}>
-                <MenuItem eventKey={3.3}>系统管理</MenuItem>
-              </LinkContainer>
-            </NavDropdown>
-          </Nav>
-          <Nav pullRight>
-            <NavItem eventKey={1} href="#">Link Right</NavItem>
-            <NavItem eventKey={2} href="#">Link Right</NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <div id="main-navigation">
+        <div className="container">
+          <ul className="main-menu">
+            <li><a href="#">首页</a>
+              <ul className="four-columns">
+                <li className="one">
+                  <h5>平台首页</h5>
+                  <ol>
+                    <li><a href="#"><i className="icon-group"></i>起始页</a></li>
+                  </ol>
+                </li>
+                <li className="one">
+                  <h5>公告信息</h5>
+                  <ol>
+                    <li><a href="#"><i className="icon-circle-arrow-left"></i>公告管理</a></li>
+                  </ol>
+                </li>
+                <li className="one">
+                  <h5>意见反馈</h5>
+                  <ol>
+                    <li><a href="#"><i className=" icon-cogs"></i>意见反馈</a></li>
+                    <li><a href="#"><i className="icon-th-large"></i>意见反馈统计</a></li>
+                  </ol>
+                </li>
+              </ul>
+            </li>
+            <li><a href="#">我的工作台</a>
+              <ul className="four-columns">
+                <li className="one">
+                  <h5>我的工作台</h5>
+                  <ol>
+                    <li><a href="#"><i className="icon-group"></i>工作台首页</a></li>
+                    <li><a href="#"><i className="icon-group"></i>手机客户端</a></li>
+                    <li><a href="#"><i className="icon-user"></i>我的传阅</a></li>
+                    <li><a href="#"><i className="icon-user"></i>我的工单</a></li>
+                    <li><a href="#"><i className="icon-user"></i>常用意见</a></li>
+                    <li><a href="#"><i className="icon-user"></i>发起工单</a></li>
+                  </ol>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
     )
   }
 }
@@ -85,3 +99,23 @@ GlobalNav.defaultProps = {
 }
 
 export default GlobalNav
+
+
+      // <div style={styles.wrapper}>
+      //   <div style={{ float: 'left' }}>
+      //     <Link to="/" style={styles.link}>首页</Link>{' '}
+      //     <Link to="/calendar" style={styles.link} activeStyle={styles.activeLink}>Calendar</Link>{' '}
+      //     <Link to="/calendar" style={styles.link} activeStyle={styles.activeLink}>室外宏站</Link>{' '}
+      //     <Link to="/grades" style={styles.link} activeStyle={styles.activeLink}>室内覆盖</Link>{' '}
+      //     <Link to="/messages" style={styles.link} activeStyle={styles.activeLink}>报表</Link>{' '}
+      //     <Link to="/user/Lyman" style={styles.link} activeStyle={styles.activeLink}>系统管理</Link>{' '}
+      //   </div>
+      //   <div style={{ float: 'right' }}>
+      //     <Link style={styles.link} to="/profile">{user.name}</Link> 
+      //       {loggedIn ? (
+      //         <Link style={styles.link} to="/logout">Log out</Link>
+      //       ) : (
+      //         <Link style={styles.link} to="/login">Sign in</Link>
+      //       )}
+      //   </div>
+      // </div>
